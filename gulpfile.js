e92b9e5e1e260ad9ac58f055e514fa8919e814a5
kidsid <gigagelashvil@gmail.com>
2016-05-29 20:48:22 +0400

    vendor added

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,6 @@ var gulp = require('gulp'),
 
 gulp.task('sass', function() {
     gulp.src('src/app.scss')
+      .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest('build/'))
 });
-
-gulp.task('watch', function() {
-    gulp.watch('src/**/*.scss', ['sass'])
-});
-
-gulp.task('default', 'watch');
